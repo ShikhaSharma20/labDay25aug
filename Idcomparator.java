21c126e86d3071d9cbc17fe7634d020740f197b6
ShikhaSharma20 <shikha.sharma80634@gmail.com>
2022-08-29 19:08:04 +0530

    push

---
+++ Idcomparator.java
@@ -0,0 +1,18 @@
+package program2;
+
+import java.util.Comparator;
+
+public class IdComparator implements Comparator <Student> {
+
+	public int compare(Student s1, Student s2) {
+		if (s1.StudentId == s2.StudentId)
+		return 0;
+		if (s1.StudentId > s2.StudentId)
+			return 1;
+		else
+			return -1;
+			
+	}
+
+
+}
---
+++ Student.java
@@ -0,0 +1,19 @@
+package program2;
+
+public class Student {
+	int StudentId;
+	String StudentName;
+	int StudentAge;
+	int StudentMarks;
+	
+	public Student(int StudentId, String StudentName, int StudentAge ,int StudentMarks) {
+		
+		this.StudentId = StudentId;
+		this.StudentName = StudentName;
+		this.StudentAge = StudentAge;
+		this.StudentMarks = StudentMarks;
+	}
+	
+	
+
+}
